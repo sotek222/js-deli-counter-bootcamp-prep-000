bd8f94e729d2542f236bcabad230cd92ed6e459c
sotek222 <sotek222@yahoo.com>
2018-10-31 23:57:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ let currentLine = function(line){
   if(line.length === 0){
     return "The line is currently empty.";
   } else {
-    
+    for(let i=0;i<line.length;i++){
+      
+    }
     // "The line is currently: 1. Ada, 2. Grace"
   }
 }
