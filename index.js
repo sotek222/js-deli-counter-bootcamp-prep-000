923ffa41203bc9148f946be47e6e853ca1b9f629
sotek222 <sotek222@yahoo.com>
2018-10-31 23:38:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 let takeANumber = function(line, name){
-  return `Welcome, ${name}. You are number ${line.length + 1} in line`;
+  return `Welcome, ${name}. You are number ${line.length + 1} in line.`;
 }
